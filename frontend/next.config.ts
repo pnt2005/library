be553061a05503278e3078cbf57c9ef9deaffaee
pnt2005 <phannthanh2005@gmail.com>
2025-07-29 21:06:30 +0700

    allow images domains

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      'upload.wikimedia.org', 
+      'www.nxbtre.com.vn',
+      'cdn1.fahasa.com',
+      'product.hstatic.net',
+      'images.thenile.io'
+    ]
+  }
 };
 
 export default nextConfig;
